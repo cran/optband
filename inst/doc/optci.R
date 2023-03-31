cec29e17d6075c04ea7d64d004fc782219b3de31
Sam Tracy <seasamgo@gmail.com>
2023-03-31 07:20:04 +0000

    version 0.2.2

--- inst/doc/optci.R
+++ inst/doc/optci.R
@@ -1,4 +1,4 @@
-## ---- echo=FALSE, message=FALSE------------------------------------------
+## ---- echo=FALSE, message=FALSE-----------------------------------------------
 library(utils)
 library(stats)
 library(LambertW)
@@ -6,7 +6,7 @@ library(survival)
 library(km.ci)
 library(optband)
 
-## ---- echo=TRUE----------------------------------------------------------
+## ---- echo=TRUE---------------------------------------------------------------
 set.seed(1990)
 N = 200
 x1 <- stats::rweibull(N, 1, 1)
@@ -16,19 +16,19 @@ x <- pmin(x1,x2)
 mydata = data.frame(stop = x, event = d)
 S = survival::survfit(Surv(x, d) ~ 1, type="kaplan-meier")
 
-## ---- fig.show='hold', fig.height=6, fig.width=6, echo=TRUE--------------
+## ---- fig.show='hold', fig.height=6, fig.width=6, echo=TRUE-------------------
 opt_S <- optband::opt.ci(S, conf.level = 0.95, fun = "surv", tl = NA, tu = NA)
 plot(opt_S, xlab="time", ylab="KM", mark.time=FALSE)
 
-## ---- fig.show='hold', fig.height=6, fig.width=6, echo=TRUE--------------
+## ---- fig.show='hold', fig.height=6, fig.width=6, echo=TRUE-------------------
 opt_H <- optband::opt.ci(S, conf.level = 0.95, fun = "cumhaz", tl = NA, tu = NA)
 plot(opt_H, fun="cumhaz", xlab="time", ylab="CH", mark.time=FALSE)
 
-## ---- fig.show='hold', fig.height=6, fig.width=6, echo=TRUE--------------
+## ---- fig.show='hold', fig.height=6, fig.width=6, echo=TRUE-------------------
 opt_H <- optband::opt.ci(S, conf.level = 0.90, fun = "cumhaz", tl = .1, tu = .9)
 plot(opt_H, fun="cumhaz", xlab="time", ylab="CH", mark.time=FALSE)
 
-## ---- fig.height=6, fig.width=6, echo=TRUE-------------------------------
+## ---- fig.height=6, fig.width=6, echo=TRUE------------------------------------
 color <- c("grey", "darkblue", "red", "green")
 plot(S, mark.time=FALSE, xlab="time", ylab="KM", col = "white")
 
@@ -42,7 +42,7 @@ lines(S,col="grey", lwd=2, mark.time=F)
 legend("topright", c("KM", "optband", "epband", "hall-wellner"), 
        lwd=2, lty=1:4, col=color)
 
-## ---- fig.height=6, fig.width=6, echo=TRUE-------------------------------
+## ---- fig.height=6, fig.width=6, echo=TRUE------------------------------------
 dat <- bladder[bladder$enum==1,]
 Hdif = survival::survfit(Surv(stop, event) ~ rx, type="kaplan-meier", data=dat)
 opt_Hdif <- optband::opt.ci(Hdif, fun="cumhaz", conf.level = 0.95, samples=2)
